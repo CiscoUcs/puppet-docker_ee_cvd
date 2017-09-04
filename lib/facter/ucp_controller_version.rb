b2c271a9f1c765b0c5714be1fcef51f5067fad43
root <root@puppet1.dcloud.cisco.com>
2017-09-04 10:23:23 +0000

    Added ucp version in fingerpring custom fact

--- lib/facter/ucp_controller_version.rb
+++ lib/facter/ucp_controller_version.rb
@@ -3,3 +3,4 @@ Facter.add('ucp_version') do
       Facter::Core::Execution.exec("docker image ls --format '{{.Tag}}' docker/ucp-auth | head -n 1")
     end
 end
+
--- lib/facter/ucp_fingerprint.rb
+++ lib/facter/ucp_fingerprint.rb
@@ -1,5 +1,7 @@
+require 'facter'
+
 Facter.add('ucp_fingerprint') do
     setcode do
-      Facter::Core::Execution.exec("docker run --rm -i --name ucp   -v /var/run/docker.sock:/var/run/docker.sock   docker/ucp fingerprint | sed 's/.*=//'")
+      Facter::Core::Execution.exec("docker run --rm -i --name ucp   -v /var/run/docker.sock:/var/run/docker.sock   docker/ucp:#{Facter.value(:ucp_version)} fingerprint | sed 's/.*=//'")
     end
 end
